1ecc717fd589d0c6f586b7813653ceb2847f0932
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-01-01 15:08:57 +0300

    Update aliases.sh to use vars
    aliases.sh
    - Update `watch-di` to use vars.sh/TAG

--- local/scripts/aliases.sh
+++ local/scripts/aliases.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+. "scripts/vars.sh"
+
 alias watch-ks="watch kubectl -n services get all"
 alias watch-kk="watch kubectl -n kafka get all"
 alias watch-ko="watch kubectl -n observability get all"
-alias watch-di="watch docker images --filter \"reference=*:tf\""
+alias watch-di="watch docker images --filter \"reference=*:$TAG\""
